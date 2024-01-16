330506ce50d63dc6fe997b8775037c2b8fd78117
CrazedBySerenity <villeisakd@gmail.com>
2024-01-16 22:50:35 +0100

    Ádded text animations

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 const textElements = document.getElementsByClassName("text__base");
 
-function playAnimation (element) {
+function PlayAnimation (element, animationClass) {
     console.log(element);
+    console.log(animationClass);
 
-    element
-    .animate(
-      [{ transform: "translate(200px, -240px) rotate(90deg)" }, { transform: "translate(800px, -700px) rotate(90deg) scale(4)" }],
-      {
-        fill: "forwards",
-        easing: "linear",
-        duration: 300,
-      },
-    );
+    element.classList.add(animationClass);
+
+    // element
+    // .animate(
+    //   [{ transform: "translate(200px, -240px) rotate(90deg)" }, { transform: "translate(800px, -700px) rotate(90deg) scale(4)" }],
+    //   {
+    //     fill: "forwards",
+    //     easing: "linear",
+    //     duration: 300,
+    //   },
+    // );
 }
